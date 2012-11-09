9e14e42eadb69413cc2971bacb66c9dbc5f2903b
Joakim Clysen <stjoakim@gmail.com>
2012-11-09 19:14:27 +0100

    nydusstorm

--- Nydus.java
+++ Nydus.java
@@ -2,21 +2,37 @@ package bot;
 
 import gameLogic.*;
 
-public class Nydus implements Brain
-{
+public class Nydus implements Brain {
 	private GameState gamestate;
 	private Snake self;
 	
-	public Direction getNextMove(Snake yourSnake, GameState gamestate)
-	{
+	public Direction getNextMove(Snake yourSnake, GameState gamestate) {
 		self = yourSnake;
 		this.gamestate = gamestate;
-		
 		Direction previousDirection = self.getCurrentDirection();
-		if (gamestate.willCollide(self, previousDirection))
-		{
-			return previousDirection.turnLeft();
+		Direction nextDirection;
+
+		int forward, left, right;
+		
+		if (gamestate.willCollide(self, previousDirection)) {
+			forward = 1000;
 		}
-		return previousDirection;
+		if (gameState.willCollide(self, previousDirection.turnLeft())) {
+			left = 1000;
+		}
+		if (gameState.willCollide(self, previousDirection.turnRight())) {
+			right = 1000;
+		}
+
+		nextDirection = self.getCurrentDirection();
+
+		if (left < forward) {
+			nextDirection = self.getCurrentDirection().turnLeft();
+		}
+		if (right < left) {
+			nextDirection = self.getCurrentDirection().turnRight();
+		}
+		
+		return nextDirection;
 	}
 }
