07ab0812bd7724f772a914e6882024206153d50b
Melburn <Melburn1@hotmail.com>
2012-11-09 19:30:44 +0100

    jag har inte testat det har, men lagt in att de vill aka mot tails i suicide-mode

--- Nydus.java
+++ Nydus.java
@@ -16,12 +16,24 @@ public class Nydus implements Brain {
 		
 		if (gamestate.willCollide(self, previousDirection)) {
 			forward = 1000;
+			Position nextPosition = previousDirection.calculateNextPosition(self.getHeadPosition());
+			ArrayList<Snake> temp = GameState.getBoard().getSquare(nextPostition).getSnakes();
+			if(temp.size() < 2 && temp.size() > 0 && temp.get(0).getTailPosition() == nextPosition)
+				forward = 998;
 		}
 		if (gameState.willCollide(self, previousDirection.turnLeft())) {
 			left = 1000;
+			Position nextPosition = previousDirection.turnLeft().calculateNextPosition(self.getHeadPosition());
+			ArrayList<Snake> temp = GameState.getBoard().getSquare(nextPostition).getSnakes();
+			if(temp.size() < 2 && temp.size() > 0 && temp.get(0).getTailPosition() == nextPosition)
+				forward = 998;
 		}
 		if (gameState.willCollide(self, previousDirection.turnRight())) {
 			right = 1000;
+			Position nextPosition = previousDirection.turnRight().calculateNextPosition(self.getHeadPosition());
+			ArrayList<Snake> temp = GameState.getBoard().getSquare(nextPostition).getSnakes();
+			if(temp.size() < 2 && temp.size() > 0 && temp.get(0).getTailPosition() == nextPosition)
+				forward = 998;
 		}
 
 		nextDirection = self.getCurrentDirection();
